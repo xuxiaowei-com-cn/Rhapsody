de2c04b77b1870b729e79f115ba3fa3179666851
徐晓伟 <xuxiaowei@xuxiaowei.com.cn>
2020-10-10 20:54:56 +0800

    公共 HTTP 3

--- src/main/java/cn/com/xuxiaowei/controller/CommonRestController.java
+++ src/main/java/cn/com/xuxiaowei/controller/CommonRestController.java
@@ -2,6 +2,7 @@ package cn.com.xuxiaowei.controller;
 
 import cn.com.xuxiaowei.util.RequestUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,7 +30,7 @@ public class CommonRestController {
      *
      * @param request  请求
      * @param response 响应
-     * @return 返回 公共 HTTP 响应结果
+     * @return 返回 公共 HTTP 1 响应结果
      */
     @RequestMapping("/1")
     public Map<String, Object> common1(HttpServletRequest request, HttpServletResponse response) {
@@ -56,11 +57,11 @@ public class CommonRestController {
     }
 
     /**
-     * 公共 HTTP 1
+     * 公共 HTTP 2
      *
      * @param request  请求
      * @param response 响应
-     * @return 返回 公共 HTTP 响应结果
+     * @return 返回 公共 HTTP 2 响应结果
      */
     @RequestMapping("/2")
     public Map<String, Object> common2(HttpServletRequest request, HttpServletResponse response) {
@@ -82,6 +83,38 @@ public class CommonRestController {
 
         map.put(CODE, CODE_OK);
         map.put(MSG, "请求成功 2");
+
+        return map;
+    }
+
+    /**
+     * 公共 HTTP 3
+     *
+     * @param request  请求
+     * @param response 响应
+     * @param a        地址参数
+     * @return 返回 公共 HTTP 3 响应结果
+     */
+    @RequestMapping("/path/{a}")
+    public Map<String, Object> commonPath(HttpServletRequest request, HttpServletResponse response, @PathVariable("a") String a) {
+
+        log.info("请求参数 {}：{}", a, RequestUtils.keyValue(request));
+
+        try {
+            log.info("请求流 {}：{}", a, RequestUtils.getInputStream(request));
+        } catch (IOException e) {
+            log.error("获取请求流异常 {}", a);
+            log.error(e.getMessage(), e);
+        }
+
+        Map<String, Object> map = new HashMap<>(4);
+        Map<String, Object> data = new HashMap<>(4);
+        map.put(DATA, data);
+
+        data.put("now " + a, LocalDateTime.now().toString());
+
+        map.put(CODE, CODE_OK);
+        map.put(MSG, "请求成功 " + a);
 
         return map;
     }
